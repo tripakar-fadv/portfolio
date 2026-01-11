cf761440080f66b0e0124ec8846d7c4b3aa8a69c
tripakar-fadv <tripakar-fadv@users.noreply.github.com>
2026-01-11 18:11:59 +0000

    deploy: 2b3d4ae8bdc82ab069ed3a588b3d51d02297855c

---
+++ _next/static/pzGN_vthWPc89qp0ylaQE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2f37a8b74d52c0a0.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
